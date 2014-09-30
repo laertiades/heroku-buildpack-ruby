fbb9ae715e9c4f4a5ba30dd4c5ff6a0a82f2424c
jesse <jesseg@sent.com>
2014-09-30 16:50:41 -0700

    added ffmpeg

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -154,6 +154,10 @@ private
   def slug_vendor_ruby
     "vendor/#{ruby_version.version_without_patchlevel}"
   end
+  
+  def slug_vendor_directory
+    "vendor"
+  end
 
   # the relative path to the vendored jvm
   # @return [String] resulting path
@@ -243,6 +247,8 @@ private
       set_env_override "GEM_PATH", "$HOME/#{slug_vendor_base}:$GEM_PATH"
       set_env_default  "LANG",     "en_US.UTF-8"
       set_env_override "PATH",     binstubs_relative_paths.map {|path| "$HOME/#{path}" }.join(":") + ":$PATH"
+      set_env_override "PATH",     "$HOME/#{slug_vendor_directory}/ffmpeg/bin:$PATH"
+      set_env_override "LD_LIBRARY_PATH", "$HOME/#{slug_vendor_directory}/ffmpeg/lib:$LD_LIBRARY_PATH"
 
       if ruby_version.jruby?
         set_env_default "JAVA_OPTS", default_java_opts
